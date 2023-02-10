064dff978d75aa75f9d4733b3143ada77893fabe
Andrea Sigurani <asiguran@e3r12p8.42roma.it>
2023-02-10 11:15:23 +0100

    primo commit

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,60 +6,63 @@
 /*   By: asiguran <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 14:55:40 by asiguran          #+#    #+#             */
-/*   Updated: 2023/02/08 16:24:21 by asiguran         ###   ########.fr       */
+/*   Updated: 2023/02/09 14:46:31 by asiguran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
-#include <stdlib.h>
-#include <stdio.h>
+/*
+** This auxiliary function counts the digits of the long integer received by
+** parameter.
+*/
 
-int	len(long nb)
+static int    ft_digit_count(long int i)
 {
-	int	len;
+    int    count;
 
-	len = 0;
-	if (nb < 0)
-	{
-		nb = nb * -1;
-		len++;
-	}
-	while (nb > 0)
-	{
-		nb = nb / 10;
-		len++;
-	}
-	return (len);
+    count = 0;
+    if (i < 0)
+    {
+        i *= -1;
+        count++;
+    }
+    while (i > 0)
+    {
+        i /= 10;
+        count++;
+    }
+    return (count);
 }
 
-char	*ft_itoa(int nb)
+char        *ft_itoa(int n)
 {
-	char	*str;
-	long	n;
-	int	i;
+    char        *str;
+    int            i;
+    long int    nb;
 
-	n = nb;
-	i = len(n);
-	if (!(str = (char*)malloc(sizeof(char) * (i + 1))))
-	return (NULL);
-	str[i--] = '\0';
-	if (n == 0)
-	{
-		str[0] = 48;
-		return (str);
-	}
-	if (n < 0)
-	{
-		str[0] = '-';
-		n = n * -1;
-	}
-	while (n > 0)
-	{
-		str[i] = 48 + (n % 10);
-        n = n / 10;
-        i--;
-	}
-	return (str);
+    nb = n;
+    i = ft_digit_count(nb);
+    if (!(str = malloc(i * sizeof(char) + 1)))
+        return (0);
+    str[i--] = 0;
+    if (nb == 0)
+    {
+        str = ft_calloc(2, sizeof(char));
+        str[0] = 48;
+    }
+    if (nb < 0)
+    {
+        str[0] = '-';
+        nb = nb * -1;
+    }
+    while (nb > 0)
+    {
+        str[i--] = nb % 10 + '0';
+        nb = nb / 10;
+    }
+    return (str);
 }
+
 //
 //int	main(void)
 //{
